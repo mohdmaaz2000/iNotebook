10dee8936c44b9e7197088665bff8ee9f5870885
Mohd Maaz <mohdmaaz.knp2019@gmail.com>
2022-05-27 15:41:48 +0530

    Storing a data into database using thunderClient

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,8 @@ const express=require('express');
 const port = 5000
 const app = express();
 
+app.use(express.json());
+
 createConnection()
 
 app.use('/api/notes', require('./routes/notes'));
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const route = express.Router();
+const User = require('../models/User');
 
 
-route.get('/',(req,res)=>{
-    obj = []
-    res.json(obj);
+route.post('/',(req,res)=>{
+    console.log(req.body);
+    const user = User(req.body);
+    user.save();
+    res.send(req.body);
 });
 
 module.exports = route;
