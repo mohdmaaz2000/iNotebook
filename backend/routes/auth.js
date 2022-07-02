86d9894b534f95d15fe0e3e913ef06322c4db00e
Mohd Maaz <mohdmaaz.knp2019@gmail.com>
2022-07-02 16:57:15 +0530

    created a login endpoint

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,9 +4,12 @@ const User = require('../models/User');
 const { body, validationResult } = require('express-validator');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const { Router } = require('express');
 
 const JWT_TOKEN = "Mynameismaaz";
 
+
+// Code for creating a new user, no login
 route.post('/createUser', [
 
   // validation for name, email and password
@@ -46,6 +49,40 @@ route.post('/createUser', [
   catch (err) {
     console.error(err.message);
     res.status(404).send("Something unexpected happened");
+  }
+});
+
+// Creating a endpoint for login a user
+route.post('/login',[
+  body('email', 'Enter valid email').isEmail()
+], async (req,res)=>{
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).send("Enter valid Credentials");
+  }
+
+  const {email,password} = req.body
+  try {
+    const user = await User.findOne({email : email});
+
+    if(!user){
+    return res.status(400).send("Enter valid Credentials");
+    }
+    const compare =await bcrypt.compare(password, user.password);
+    if(!compare){
+      return res.status(400).send("Enter valid Credentials");
+    }
+    
+    const data = {
+      user : {
+        id: user.id
+      }
+    }
+    const token = jwt.sign(data,JWT_TOKEN);
+    res.send(token);
+    
+  } catch (error) {
+    res.status(400).send("Enter valid Credentials");
   }
 });
 
