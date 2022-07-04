79f129aaf27ce2838f051080313f031a5bb8a5be
Mohd Maaz <mohdmaaz.knp2019@gmail.com>
2022-07-04 13:02:03 +0530

    Added the middleware to decde the user from jwt token

---
+++ backend/middleware/fetchUser.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+const JWT_TOKEN = "Mynameismaaz";
+
+const fetchUser = (req,res,next) =>{
+//    Get the user from the jwt Token and add id to req object
+    const token = req.header('auth-token');
+    if(!token){
+        res.status(401).send({error:"Please enter the valid token"});
+    }
+
+    try {
+        const data = jwt.verify(token,JWT_TOKEN);
+        req.user = data.user;
+        next();
+    } catch (error) {
+        res.status(401).send({error:"Please enter the valid token"});
+    }
+
+}
+
+module.exports = fetchUser;
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,12 +4,12 @@ const User = require('../models/User');
 const { body, validationResult } = require('express-validator');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const { Router } = require('express');
+const fetchuser = require('../middleware/fetchUser');
 
 const JWT_TOKEN = "Mynameismaaz";
 
 
-// Code for creating a new user, no login
+//Route 1 :  Code for creating a new user usingh post, no login  (/api/auth/createUser)
 route.post('/createUser', [
 
   // validation for name, email and password
@@ -52,7 +52,7 @@ route.post('/createUser', [
   }
 });
 
-// Creating a endpoint for login a user
+//Route 2 :  Creating a endpoint for login a user using post (/api/auth/login)
 route.post('/login',[
   body('email', 'Enter valid email').isEmail()
 ], async (req,res)=>{
@@ -85,5 +85,16 @@ route.post('/login',[
     res.status(400).send("Enter valid Credentials");
   }
 });
+
+// Route 3 : Creating a middleware for decoding jwt token using post (/api/auth/fetchUser)
+route.post('/fetchUser',fetchuser ,async (req,res)=>{
+  try {
+    const UserId = req.user.id;
+    const user = await User.findById(UserId).select("-password");
+    res.send(user);
+  } catch (error) {
+    res.status(500).send("Internal Server Error");
+  }
+}) 
 
 module.exports = route;
